10a0e1d587ef0d8327dec99f5ad8d79e36f255ac
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-01-04 11:53:58 +0100

    task 2 almost completed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//task 1
+
 // console.log("Nsebo");
 // console.log(7 + 8);
 
@@ -7,3 +9,10 @@ console.log("Monday");
 console.log(3 * 6);
 //Using the alert() method, alert your name
 alert("Nsebo");
+
+//task 2
+var animal = "frog";
+var delivered = true;
+var score = 12;
+var fullName = Nsebo;
+//console.log(delivered);

cd4543398dfbee5924a2862f407919366587ce7b
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-01-05 14:01:47 +0100

    Today's class activities.

--- script.js
+++ script.js
@@ -46,3 +46,56 @@ console.log(lightIsOn);
 
 var nseboIsGirl = 23;
 console.log(nseboIsGirl);
+
+//class activities day 3
+var firstNo = 10;
+var secondNo = "10";
+console.log(firstNo === secondNo); //false
+
+var myNumber = 7;
+var myString = "dog";
+
+console.log(myNumber > 8);
+console.log(myNumber <= 8);
+console.log(myNumber === "dog");
+console.log(myNumber !== "cat");
+// declare a logged in variable
+// check if the user is logged in
+// if the user is logged in i will show a nice message in the console
+// else if the user is not logged in i will show a message too
+
+var isLoggedIn = true;
+
+if (isLoggedIn === true) {
+  console.log("The user is logged in");
+} else {
+  console.log("The user is not logged in");
+}
+
+var inputIsValid = false;
+
+// if (inputIsVaild === false); {
+// console.log ("please fill in the email field");
+// } else {
+//console.log();
+// }
+
+var myAge = 32;
+
+if (myAge > 34) {
+  console.log("i am young");
+} else if (myAge === 32) {
+  console.log("I am stil very young");
+}
+
+var color = "red";
+switch (color) {
+  case "red":
+    console.log("The chosen color is red");
+    break;
+  case "blue":
+    console.log("The chosen color is blue");
+    break;
+  default:
+    console.log("The color is not blue, red, green...");
+}

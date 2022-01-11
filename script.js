a1114b74dc7a784ed95c61e06083f8f10e6e9451
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-01-11 22:02:07 +0100

    task 4 of lesson task 1 completed

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-//task 1
-
-// console.log("Nsebo");
-// console.log(7 + 8);
-
-// current day of the week
-// console.log("Monday");
-// Console log 3 multiplied by 6
-// console.log(3 * 6);
-//Using the alert() method, alert your name
-// alert("Nsebo");
-
-//task 2
-// var animal = "frog";
-// console.log(animal);
-
-// var delivered = true;
-// console.log(delivered);
-
-// var score = 12;
-// console.log(score);
-
-// var fullName = "John" + "Smith";
-// console.log(fullName);
-
-// var nameType = typeof fullName;
-// console.log(nameType);
-
 // class activities
 // var pet = "cat";
 // console.log(pet);
@@ -98,25 +70,6 @@
 // break;
 // default:
 // console.log("The color is not blue, red, green...");
-// }
-
-// task 3
-// var age = 10;
-
-// if (age >= 7) {
-// console.log("Greater than or equal to 7");
-// } else {
-//console.log("less than 7");
-// }
-
-// var animal = "dog";
-
-// if (animal === "parrot") {
-// console.log("The animal has two legs");
-// } else if (animal === "cat") {
-//  console.log("The animal has four legs");
-// } else if (animal != "parrot" || "cat") {
-// console.log("The animal has an unknown amount of legs");
 // }
 
 //class activities day 4 loop
---
+++ tasks.js
@@ -0,0 +1,71 @@
+//Lesson Task 1
+
+//task 1
+
+// console.log("Nsebo");
+// console.log(7 + 8);
+
+// current day of the week
+// console.log("Monday");
+// Console log 3 multiplied by 6
+// console.log(3 * 6);
+//Using the alert() method, alert your name
+// alert("Nsebo");
+
+//task 2
+// var animal = "frog";
+// console.log(animal);
+
+// var delivered = true;
+// console.log(delivered);
+
+// var score = 12;
+// console.log(score);
+
+// var fullName = "John" + "Smith";
+// console.log(fullName);
+
+// var nameType = typeof fullName;
+// console.log(nameType);
+
+// task 3
+// var age = 10;
+
+// if (age >= 7) {
+// console.log("Greater than or equal to 7");
+// } else {
+//console.log("less than 7");
+// }
+
+// var animal = "dog";
+
+// if (animal === "parrot") {
+// console.log("The animal has two legs");
+// } else if (animal === "cat") {
+//  console.log("The animal has four legs");
+// } else if (animal != "parrot" || "cat") {
+// console.log("The animal has an unknown amount of legs");
+// }
+
+// 4
+// Write a for loop that logs a number from 1 to 5
+for (var count = 1; count <= 5; count++) {
+  console.log(count);
+}
+
+//Write a for loop that logs a number from 7 to 16
+for (var count = 7; count <= 16; count++) {
+  console.log(count);
+}
+// Given this array var animals = [“dog”, “cat”, “frog”]; use a for loop to console log each animal in the array.
+
+var animals = ["dog", "cat", "frog"];
+var numberOfAnimals = animals.length;
+
+for (var i = 0; i <= animals.length; i++) {
+  console.log(animals[i]);
+}
+
+//Lesson Task 2
+
+//
